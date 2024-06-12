bae3688810a09665776a5bb66e08a042055141f7
Feldwor <BoQsc@users.noreply.github.com>
2024-06-13 00:14:24 +0300

    Initial preparations for tomorrow.

--- server.py
+++ server.py
@@ -15,23 +15,36 @@ async def index(request):
 async def posting(request):
     return web.FileResponse(library.relativePath + "/library/posting.html")
 
+# TODO: How to make it without redirect to another web page?
+# TODO: How to make it with Websockets?
+# TODO: How to make authentication?
+# TODO: How to upload images?
+# TODO: How to provide Web API for interacting as user or bot in a chat?
+# TODO: How to do end-to-end encryption?
+# TODO: How to do a decentralized database?
+# TODO: How to make a master server if it's a chat platform?
 
 async def save_text(request):
     data = await request.post()
     text = data.get('text')
 
     if text:
-        async with aiosqlite.connect(library.relativePath + '/serverdatabase.db') as db:
-            import os
-            db_path = os.path.abspath(library.relativePath + '/serverdatabase.db')
-            print(f"Database will be created at: {db_path}")
-            print(f"Current working directory: {os.getcwd()}")
+        async with aiosqlite.connect(databasefile) as db:
             await db.execute('CREATE TABLE IF NOT EXISTS texts (id INTEGER PRIMARY KEY, text TEXT)')
             await db.execute('INSERT INTO texts (text) VALUES (?)', (text,))
             await db.commit()
             return web.Response(text="Text saved successfully")
 
 if __name__ == '__main__':
+
+    global databasefile
+    databasefile = library.relativePath + '/serverdatabase.db' 
+    
+    import os
+    db_path = os.path.abspath(databasefile)
+    print(f"Database will be created at: {db_path}")
+    print(f"Current working directory: {os.getcwd()}")
+
     app = web.Application()
     app.add_routes([web.get('/', index)])
     app.add_routes([web.get('/posting', posting)])
