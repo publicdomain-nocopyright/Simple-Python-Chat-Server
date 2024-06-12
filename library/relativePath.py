6ec3e4c25d0cf02fab62d4e3a978d2b3a2ee485a
Feldwor <BoQsc@users.noreply.github.com>
2024-06-12 20:31:33 +0300

    Serving the index.html page.

--- library/relativePath.py
+++ library/relativePath.py
@@ -1,2 +1,6 @@
-def getCurrentProjectPath():
-    pass
+import os
+import sys
+
+
+relative_path = os.path.abspath(os.path.dirname(sys.argv[0]))
+sys.modules[__name__] = relative_path
--- server.py
+++ server.py
@@ -5,14 +5,14 @@ sys.dont_write_bytecode = True
 
 from aiohttp import web
 
-
-
 import library.header 
+import library.relativePath
 library.header
-
-
+#print(library.relativePath.getCurrentProjectPath())
+import library.relativePath
+print(library.relativePath)
 async def index(request):
-    return web.FileResponse("./index.html")
+    return web.FileResponse(library.relativePath + "/index.html")
 
 app = web.Application()
 app.add_routes([web.get('/', index)])
